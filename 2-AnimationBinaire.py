754effb7b9b0b84c37b705d780163bb4ec628188
Jean Durgeaud <53953178+OneGuyy@users.noreply.github.com>
2019-09-26 11:43:03 +0200

    nouvelle fonction countdown - optimisée et claire
    + Changement de la fonction convert par la fonction récursive

--- 2-AnimationBinaire.py
+++ 2-AnimationBinaire.py
@@ -1,25 +1,6 @@
-def convert(x):
-        compteur = 0 #Le compteur pour 2^y
-        puissanceDeux = 1
-        while(x > puissanceDeux): #Tant que x est plus petit que la fonction 2^y, la boucle tourne
-            puissanceDeux *= 2    #2^(y+1)
-            if(x < puissanceDeux):  #2ème vérification pour éviter que le compteur compte trop
-                puissanceDeux /= 2
-                break               #Si x est plus petit que la fonction, alors on arrête la boucle
-            compteur += 1         #On ajoute 1 au compteur, celui ci servira pour calculer le nombre en binaire
-        resultat = 0 #Variable qui stockera la valeur convertie
-        ajout = puissanceDeux
-        while(compteur != 0):
-            if(x >= puissanceDeux):
-                resultat += 10**compteur
-            else:
-                puissanceDeux -= ajout
-            ajout = ajout/2
-            puissanceDeux += ajout
-            compteur -= 1
-        if(x%2==1):
-            return resultat+1
-        return resultat
+def convert(x, resultat = ''):
+    if x == 0: return "0b" + resultat[::-1]
+    else: return convert(x//2, resultat + str(x%2)) 
 
 def countdown(x):
 	listValues = []
@@ -27,8 +8,15 @@ def countdown(x):
 		listValues.append(i)
 	listValues.reverse()
 	for val in listValues:
-		print(convert(val))
+		print(convert(val), end=" ; ")
+
+def countdown2(x):
+    for i in range(x, -1, -1):
+        print(convert(i), end=" ; ")
 
 countdown(10)
+print("")
+countdown2(10)
+
 
 input("")
